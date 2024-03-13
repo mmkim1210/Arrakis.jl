38dc476e6b9e5cee16bf11ea6ca50f8058b8503e
minsoo <mmkim1210@gmail.com>
2024-03-12 17:28:39 -0700

    rename

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
-using Dune
+using Arrakis
 using Test
 
-@testset "Dune.jl" begin
-    @test Dune.quotes[467] === """“Data,” Paul said. “I need more data.”"""
+@testset "Arrakis.jl" begin
+    @test Arrakis.quotes[467] === """“Data,” Paul said. “I need more data.”"""
 end
